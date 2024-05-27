c74faba989fa1935148ceb923c73ba513de7c2a1
YOSHIDA-HARUNA <yoshida.haruna.qy@mynavi.jp>
2024-05-27 06:18:18 +0000

    ｔ

---
+++ yoshida/application/flask_blog/__init__.py
@@ -0,0 +1,11 @@
+#hello.py
+#アマゾンのサーバーにあるファイルをローカルでのみ公開しているイメージ
+from flask import Flask
+app = Flask(__name__)
+
+app.config.from_object("flask_blog.config")
+import flask_blog.views
+
+
+
+#import flask_blog.views
