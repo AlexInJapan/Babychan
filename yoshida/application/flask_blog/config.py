a43a22f40f35482426a75b5fe16cb4642e2d8331
YOSHIDA-HARUNA <yoshida.haruna.qy@mynavi.jp>
2024-05-29 08:35:47 +0000

    もう無理

--- yoshida/application/flask_blog/config.py
+++ yoshida/application/flask_blog/config.py
@@ -3,15 +3,15 @@ SECRET_KEY = "secret key"
 USERNAME = "john"#〇！
 PASSWORD = "due123"
 
-#SQLARCHEMY_DATABASE_URI = "sqlite://flask_blog.db"
-#SQLARCHEMY_TRACK_MODIFICATIONS = True
+#SQLALCHEMY_DATABASE_URI = "sqlite://flask_blog.db"
+#SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 import os
 
-SQLARCHEMY_DATABASE_URI = "mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8".format(**{
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8".format(**{
     "user":os.getenv("DB_USER","root"),
     "password":os.getenv("DB_PASSWORD","mysql"),
     "host":os.getenv("DB_HOST","localhost"),
     "database":os.getenv("DB_DATABASE","ENSHU")
     })
-SQLARCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
