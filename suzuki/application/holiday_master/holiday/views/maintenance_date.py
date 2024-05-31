d83e9f8c1ffb8cb02f20728b3934942b0afcceeb
suzuki-sota-ok <suzuki.sota.ok@mynavi.jp>
2024-05-31 08:12:35 +0000

    祝日マスタ 入力時のバリデーション機能を実装

--- suzuki/application/holiday_master/holiday/views/maintenance_date.py
+++ suzuki/application/holiday_master/holiday/views/maintenance_date.py
@@ -16,55 +16,69 @@ def update_holiday():
 
     # 入力画面から祝日日付を取得
     holiday_date = request.form['holiday']
-    holiday_date = datetime.strptime(holiday_date, "%Y-%m-%d").date()
 
     # 入力画面から祝日名を取得
     holiday_text = request.form['holiday_text']
 
-    # 入力値をHolidayインスタンスに代入
-    holi_dict = Holiday(
-                holi_date=holiday_date,
-                holi_text=holiday_text)
-
-    # メッセージIDを格納する変数を宣言
-    msg_id = ""
-
-    # 祝日テーブルから入力に対応する列を取得
-    holiday = Holiday.query.get(holiday_date)
-
-    # 祝日テーブルに入力した日付がない場合
-    if holiday is None:
-        if request.form["button"] == "insert_update":
-            new_holiday = holi_dict
-            db.session.add(new_holiday)
-            db.session.commit()
-            msg_id = "IO1"
-        elif request.form["button"] == "delete":
-            flash(f"{holiday_date}は、祝日マスタに登録されていません")
-            return redirect('/')
-
-    # 祝日テーブルに入力した日付がある場合
-    elif holiday.holi_date == holiday_date:
-        # 更新処理
-        if request.form["button"] == "insert_update":
-            update_holiday = holi_dict
-            db.session.merge(update_holiday)
-            db.session.commit()
-            msg_id = "IO2"
-        # 削除処理
-        elif request.form["button"] == "delete":
-            db.session.delete(holiday)
-            db.session.commit()
-            msg_id = "IO3"
-
-    # 祝日マスタが登録されていない場合
+    if holiday_date == "" and holiday_text == "":
+        flash("日付とテキストを入力してください")
+        return redirect('/')
+    elif holiday_date == "":
+        flash("日付を入力してください")
+        return redirect('/')
+    elif holiday_text == "":
+        flash("テキストを入力してください")
+        return redirect('/')
+    elif len(holiday_text) > 20:
+        flash("テキストは20文字以内で入力してください")
+        return redirect('/')
     else:
-        msg_id = "IO4"
+        # 祝日日付をstr型からdate型に変換
+        holiday_date = datetime.strptime(holiday_date, "%Y-%m-%d").date()
+        # 入力値をHolidayインスタンスに代入
+        holi_dict = Holiday(
+                    holi_date=holiday_date,
+                    holi_text=holiday_text)
 
-    return render_template(
-            "result.html",
-            holiday_date=holiday_date,
-            holiday_text=holiday_text,
-            msg_id = msg_id
-        )
+        # メッセージIDを格納する変数を宣言
+        msg_id = ""
+
+        # 祝日テーブルから入力に対応する列を取得
+        holiday = Holiday.query.get(holiday_date)
+
+        # 祝日テーブルに入力した日付がない場合
+        if holiday is None:
+            if request.form["button"] == "insert_update":
+                new_holiday = holi_dict
+                db.session.add(new_holiday)
+                db.session.commit()
+                msg_id = "IO1"
+            elif request.form["button"] == "delete":
+                flash(f"{holiday_date}は、祝日マスタに登録されていません")
+                return redirect('/')
+
+        # 祝日テーブルに入力した日付がある場合
+        elif holiday.holi_date == holiday_date:
+            # 更新処理
+            if request.form["button"] == "insert_update":
+                update_holiday = holi_dict
+                db.session.merge(update_holiday)
+                db.session.commit()
+                msg_id = "IO2"
+            # 削除処理
+            elif request.form["button"] == "delete":
+                db.session.delete(holiday)
+                db.session.commit()
+                msg_id = "IO3"
+
+        # 祝日マスタが登録されていない場合
+        else:
+            msg_id = "IO4"
+
+        return render_template(
+                "result.html",
+                holiday_date=holiday_date,
+                holiday_text=holiday_text,
+                msg_id = msg_id
+            )
 
